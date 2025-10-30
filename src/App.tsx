8b6bd847dd8c91941cf11d9e9ee1e427983b809b
Drakiinoxx <nicolas.malafosse@gmail.com>
2025-10-30 13:27:32 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
+import Homepage from "./pages/Homepage";
 
 function App() {
-
-
   return (
     <>
-
+      <Homepage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+function Homepage() {
+  return (
+    <nav>
+        <img src="" alt="" />
+        <h1 className="flex justify-center text-7xl mt-105"> <Link > Star Wars navigation</Link> </h1>
+    </nav>
+  )
+}
+export default Homepage;
