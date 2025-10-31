c842ef5b025aa3e6b047b4c9c499e6c4c01fe170
korpiklaanix <anaisberthome@outlook.fr>
2025-10-31 10:18:37 +0100

    ajout boutons anais

--- src/pages/CharacterDetails.tsx
+++ src/pages/CharacterDetails.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import { useParams, Link } from "react-router";
 import "../styles/CharacterDetails.css";
+import { useNavigate } from "react-router";
 
 function CharacterDetails() {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("https://miadil.github.io/starwars-api/api/all.json")
@@ -18,26 +20,41 @@ function CharacterDetails() {
   if (!character) return null;
 
   return (
-    <div className="vh">
-      <div className="flex">
-        <div className="detail-wrapper">
-          <img src={character.image} alt={character.name} />
-          <div>
-            <h2>{character.name}</h2>
-            <p>Espèce : {character.species ?? "—"}</p>
-            <p>Monde natal : {character.homeworld ?? "—"}</p>
-            <p>Genre : {character.gender ?? "—"}</p>
-            <p>Taille : {character.height ?? "?"} m</p>
-            <p>Poids : {character.mass ?? "?"} kg</p>
-            {character.wiki && (
-              <a href={character.wiki} target="_blank" rel="noreferrer">
-                Voir le wiki ↗
-              </a>
-            )}
+    <>
+      <div className="vh">
+        <div className="center">
+          <button
+            type="button"
+            className=" stpsauvemoi border-2 border-amber-400 bg-amber-400/20 text-amber-300 text-2xl font-semibold rounded-full flex justify-center items-center  shadow-[0_0_20px_rgba(255,191,0,0.5)] hover:bg-amber-400 hover:text-black hover:shadow-[0_0_40px_rgba(255,191,0,0.9)] transition-all duration-300 tracking-widest uppercase"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Retour
+          </button>
+        </div>
+        <div className="">
+          <div className="flex">
+            <div className="detail-wrapper">
+              <img src={character.image} alt={character.name} />
+              <div>
+                <h2>{character.name}</h2>
+                <p>Espèce : {character.species ?? "—"}</p>
+                <p>Monde natal : {character.homeworld ?? "—"}</p>
+                <p>Genre : {character.gender ?? "—"}</p>
+                <p>Taille : {character.height ?? "?"} m</p>
+                <p>Poids : {character.mass ?? "?"} kg</p>
+                {character.wiki && (
+                  <a href={character.wiki} target="_blank" rel="noreferrer">
+                    Voir le wiki ↗
+                  </a>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
