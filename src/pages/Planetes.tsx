b3d806aa098e5957e1511b8566335c41188282e2
korpiklaanix <anaisberthome@outlook.fr>
2025-10-31 09:08:10 +0100

    pb

--- src/pages/Planetes.tsx
+++ src/pages/Planetes.tsx
@@ -1,16 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 
 function Planetes() {
-  const { systemName } = useParams(); 
+  const { systemName } = useParams();
   const [system, setSystem] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3310/solar-system")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((data) => {
         const selectedSystem = data.find(
-          (s) => s.name === decodeURIComponent(systemName!)
+          (s) => s.name === decodeURIComponent(systemName!),
         );
         setSystem(selectedSystem);
       });
@@ -18,21 +18,24 @@ function Planetes() {
 
   return (
     <div className="solar-container">
-        {system ? (
-      <div className="systems-grid">
-        {system.planets.map((planet) => (
-          <div key={planet.name} className="system-card">
-            <img src={planet.image} alt={planet.name} className="system-image" />
-            <h3>{planet.name}</h3>
-          </div>
-        ))}
-      </div>
-        ) : (
-            <p>Chargement ...</p>
-        )}
+      {system ? (
+        <div className="systems-grid">
+          {system.planets.map((planet) => (
+            <div key={planet.name} className="system-card">
+              <img
+                src={planet.image}
+                alt={planet.name}
+                className="system-image"
+              />
+              <h3>{planet.name}</h3>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Chargement ...</p>
+      )}
     </div>
   );
 }
 
 export default Planetes;
-
